3f5b6b5b765ec24422cf986ad37415d0aaa65306
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-02-02 19:28:08 +0100

    feat: initialize Angular application with basic structure and config

--- Sandbox.AppHost/Program.cs
+++ Sandbox.AppHost/Program.cs
@@ -5,6 +5,10 @@ var cache = builder.AddRedis("cache");
 var apiService = builder.AddProject<Projects.Sandbox_ApiService>("apiservice")
     .WithReplicas(2);
 
+var angularApp = builder
+    .AddNpmApp("angularfrontend", "../Sandbox.AngularApp")
+    .WithHttpEndpoint(env: "PORT");
+
 var apiGateway = builder.AddProject<Projects.Sandbox_ApiGateway>("apigateway")
     .WithReference(apiService);
 
