6c2d6c08eeede245b0fc5e563d3912074360ab45
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-07-06 09:20:21 +0200

    ci: run build & tests during CI ⚠️

--- Sandbox.EndToEndTests/playwright.config.ts
+++ Sandbox.EndToEndTests/playwright.config.ts
@@ -75,9 +75,9 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: {
+		command: 'cd .. && dotnet run --project ./Sandbox.AppHost',
+		url: 'http://localhost:5165',
+		reuseExistingServer: !process.env.CI,
+	},
 });
