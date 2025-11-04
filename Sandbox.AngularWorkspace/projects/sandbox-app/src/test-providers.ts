99b9146142ee5fd55c40efbb90cce04c8162aa4a
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-11-04 20:33:02 +0100

    fix tests

--- Sandbox.AngularWorkspace/projects/sandbox-app/src/test-providers.ts
+++ Sandbox.AngularWorkspace/projects/sandbox-app/src/test-providers.ts
@@ -1,11 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideZonelessChangeDetection } from '@angular/core';
-import { provideEventPlugins } from '@sandbox-app/shared/event-managers';
 
-export default [
-	provideZonelessChangeDetection(),
-	provideHttpClient(),
-	provideHttpClientTesting(),
-	provideEventPlugins(),
-];
+export default [provideZonelessChangeDetection(), provideHttpClient(), provideHttpClientTesting()];
