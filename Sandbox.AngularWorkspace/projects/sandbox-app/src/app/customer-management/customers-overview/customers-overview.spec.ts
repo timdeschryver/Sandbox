7ccbdb3601121ff4c54e40b66c965007463411f9
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2025-11-05 20:06:09 +0100

    test: enable vitest snapshot testing 📷

--- Sandbox.AngularWorkspace/projects/sandbox-app/src/app/customer-management/customers-overview/customers-overview.spec.ts
+++ Sandbox.AngularWorkspace/projects/sandbox-app/src/app/customer-management/customers-overview/customers-overview.spec.ts
@@ -1,26 +1,29 @@
-import { it } from 'vitest';
+import { expect, it } from 'vitest';
 import { HttpTestingController } from '@angular/common/http/testing';
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
 import { type CustomerOverviewResponse } from '@sandbox-app/customer-management/models';
 import CustomersOverview from './customers-overview';
 import { TestBed } from '@angular/core/testing';
-import { generateUuid } from '@sandbox-app/shared/functions';
 
 it('renders the component with customers table', async () => {
 	const mockCustomers: CustomerOverviewResponse[] = [
-		{ id: generateUuid(), firstName: 'John', lastName: 'Doe' },
-		{ id: generateUuid(), firstName: 'Jane', lastName: 'Smith' },
+		{ id: 'a8574564-ef6f-42da-9393-67d7e466e8c8', firstName: 'John', lastName: 'Doe' },
+		{ id: 'd654bad5-19e1-4728-b019-37d30358524f', firstName: 'Jane', lastName: 'Smith' },
 	];
 
-	const { mockRequest, container: _container } = await setup();
+	const { mockRequest, container } = await setup();
 
 	await mockRequest(mockCustomers);
 
-	// TODO: can't use snapshot testing?
-	// expect(container).toMatchInlineSnapshot();
+	expect(screen.getByRole('row', { name: /John Doe/i })).toBeInTheDocument();
+	expect(screen.getByRole('row', { name: /Jane Smith/i })).toBeInTheDocument();
+
+	expect(container).toMatchSnapshot();
 });
 async function setup() {
-	const { fixture, container } = await render(CustomersOverview);
+	const { fixture, container } = await render(CustomersOverview, {
+		removeAngularAttributes: true,
+	});
 
 	const httpMock = TestBed.inject(HttpTestingController);
 	return {
