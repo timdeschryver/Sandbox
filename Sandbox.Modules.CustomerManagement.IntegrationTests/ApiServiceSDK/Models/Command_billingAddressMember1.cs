2861f77a21503467204496353cdcc7a2c47127e9
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2026-01-06 19:49:38 +0000

    fix errors

--- Sandbox.Modules.CustomerManagement.IntegrationTests/ApiServiceSDK/Models/Command_billingAddressMember1.cs
+++ Sandbox.Modules.CustomerManagement.IntegrationTests/ApiServiceSDK/Models/Command_billingAddressMember1.cs
@@ -28,7 +28,7 @@ namespace ApiServiceSDK.Models
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
         public static global::ApiServiceSDK.Models.Command_billingAddressMember1 CreateFromDiscriminatorValue(IParseNode parseNode)
         {
-            _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
+            if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
             return new global::ApiServiceSDK.Models.Command_billingAddressMember1();
         }
         /// <summary>
@@ -47,7 +47,7 @@ namespace ApiServiceSDK.Models
         /// <param name="writer">Serialization writer to use to serialize this model</param>
         public virtual void Serialize(ISerializationWriter writer)
         {
-            _ = writer ?? throw new ArgumentNullException(nameof(writer));
+            if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
             writer.WriteAdditionalData(AdditionalData);
         }
     }
