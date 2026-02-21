84e5a24b3acccc381dbc767e8f95062d18dbfd4c
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2026-02-21 21:16:46 +0100

    test

--- Sandbox.EndToEndTests/playwright.config.ts
+++ Sandbox.EndToEndTests/playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 		command: 'dotnet run --project ./Sandbox.AppHost',
 		url: config.applicationUrl + '/health',
 		reuseExistingServer: !process.env.CI,
-		timeout: 60000 * 3,
+		timeout: 60000 * 10,
 		ignoreHTTPSErrors: true,
 		stdout: 'pipe',
 		stderr: 'pipe',
